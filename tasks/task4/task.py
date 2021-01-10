64f837cf62d2d2e63bb73606ebc762f77ee25dfb
mariiabav <61200015+mariiabav@users.noreply.github.com>
2021-01-10 17:24:24 +0300

    Add files via upload

---
+++ tasks/task4/task.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+def task4(file_names: List[str]):
+    """
+    Parameters:
+    :type file_names (List[str]): names of students files
+    :return: names of teacher's files
+    """
+    pass
--- tasks/test_public.py
+++ tasks/test_public.py
@@ -2,48 +2,31 @@ from typing import List
 
 import pytest
 
-from .task import task3
+from .task import task4
 
 
 class Case:
-    def __init__(self, name: str, n: int, edges: List[List[int]], probabilities: List[float], start: int, end: int,
-                 answer: float):
+    def __init__(self, name: str, file_names: List[str], answer: List[str]):
         self._name = name
-        self.n = n
-        self.edges = edges
-        self.probabilities = probabilities
-        self.start = start
-        self.end = end
+        self.file_names = file_names
         self.answer = answer
 
     def __str__(self) -> str:
-        return 'task3_test_{}'.format(self._name)
+        return 'task4_test_{}'.format(self._name)
 
 
 TEST_CASES = [
     Case(
         name='case1',
-        n=3,
-        edges=[
-            [0, 1],
-            [1, 2],
-            [0, 2]
-        ],
-        probabilities=[0.5, 0.5, 0.2],
-        start=0,
-        end=2,
-        answer=0.25000,
+        file_names=["pes", "fifa", "gta", "pes(2019)"],
+        answer=["pes", "fifa", "gta", "pes(2019)"],
     ),
 ]
 
 
 @pytest.mark.parametrize('case', TEST_CASES, ids=str)
-def test_task3(case: Case) -> None:
-    answer = task3(
-        n=case.n,
-        edges=case.edges,
-        probabilities=case.probabilities,
-        start=case.start,
-        end=case.end,
+def test_task4(case: Case) -> None:
+    answer = task4(
+        file_names=case.file_names,
     )
     assert answer == case.answer
