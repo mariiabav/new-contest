da0cf88e078d1b68f175aaf3bb96f21c1a1812eb
mariiabav <61200015+mariiabav@users.noreply.github.com>
2021-01-10 17:23:34 +0300

    Add files via upload

---
+++ tasks/task1/task.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def task1(n: int, m: int, edges: List[List[int]]):
+    """
+    Parameters:
+    :type n (int): number of stations
+    :type m (int): number of moves
+    :type edges (List[List[int]]): new stations information
+    :return: required amount of metro stations
+    """
+    pass
