7bdf8e823b42f94a7659d618cab7041bc9e9ebb4
mariiabav <61200015+mariiabav@users.noreply.github.com>
2021-01-10 17:24:47 +0300

    Add files via upload

---
+++ tasks/task5/task.py
@@ -0,0 +1,7 @@
+def task5(expression: str):
+    """
+    Parameters:
+    :type expression (str): boolean expression
+    :return: result of expression evaluating
+    """
+    pass
--- tasks/test_public.py
+++ tasks/test_public.py
@@ -1,32 +1,30 @@
-from typing import List
-
 import pytest
 
-from .task import task4
+from .task import task5
 
 
 class Case:
-    def __init__(self, name: str, file_names: List[str], answer: List[str]):
+    def __init__(self, name: str, expression: str, answer: bool):
         self._name = name
-        self.file_names = file_names
+        self.expression = expression
         self.answer = answer
 
     def __str__(self) -> str:
-        return 'task4_test_{}'.format(self._name)
+        return 'task5_test_{}'.format(self._name)
 
 
 TEST_CASES = [
     Case(
         name='case1',
-        file_names=["pes", "fifa", "gta", "pes(2019)"],
-        answer=["pes", "fifa", "gta", "pes(2019)"],
+        expression='|(&(1,0,1),!(1))',
+        answer=False,
     ),
 ]
 
 
 @pytest.mark.parametrize('case', TEST_CASES, ids=str)
-def test_task4(case: Case) -> None:
-    answer = task4(
-        file_names=case.file_names,
+def test_task5(case: Case) -> None:
+    answer = task5(
+        expression=case.expression,
     )
     assert answer == case.answer
