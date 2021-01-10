7e59f6e2e617613a7840c4bab0f43e2c84999c97
mariiabav <61200015+mariiabav@users.noreply.github.com>
2021-01-10 19:41:42 +0300

    Add files via upload

---
+++ tasks/task9/task.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def task9(n: int, plots: List[List[int]]):
+    """
+    :type n (int):  number of plots
+    :type plots (List[List[int]]): plots expansion information
+    :return: required number of plots after consolidation
+    """
+    pass
+
+
