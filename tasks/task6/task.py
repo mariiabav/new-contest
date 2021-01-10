3d8d1bf4dbc2159db5344de54a43d7c966497b09
mariiabav <61200015+mariiabav@users.noreply.github.com>
2021-01-10 17:25:21 +0300

    Add files via upload

---
+++ tasks/task6/task.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+def task6(n: int, cost: List[int]):
+    """
+    :type n (int): number of skills
+    :type cost (List[int]): costs of skills
+    :return: required station numbers
+    """
+    pass
--- tasks/test_public.py
+++ tasks/test_public.py
@@ -1,30 +1,35 @@
+from typing import List
+
 import pytest
 
-from .task import task5
+from .task import task6
 
 
 class Case:
-    def __init__(self, name: str, expression: str, answer: bool):
+    def __init__(self, name: str, n: int, cost: List[int], answer: str):
         self._name = name
-        self.expression = expression
+        self.n = n
+        self.cost = cost
         self.answer = answer
 
     def __str__(self) -> str:
-        return 'task5_test_{}'.format(self._name)
+        return 'task6_test_{}'.format(self._name)
 
 
 TEST_CASES = [
     Case(
         name='case1',
-        expression='|(&(1,0,1),!(1))',
-        answer=False,
+        n=9,
+        cost=[4, 3, 2, 5, 6, 7, 2, 5, 5],
+        answer='7772',
     ),
 ]
 
 
 @pytest.mark.parametrize('case', TEST_CASES, ids=str)
-def test_task5(case: Case) -> None:
-    answer = task5(
-        expression=case.expression,
+def test_task6(case: Case) -> None:
+    answer = task6(
+        n=case.n,
+        cost=case.cost,
     )
     assert answer == case.answer
